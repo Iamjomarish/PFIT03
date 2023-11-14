a3e145b5fb5ab394e12d2b536fef5fe6ea44e15a
Iamjomarish <146721414+Iamjomarish@users.noreply.github.com>
2023-11-14 12:33:24 +0000

    Add files via upload

---
+++ js/slider.js
@@ -0,0 +1,12 @@
+
+            const slider = document.querySelectorAll('.slider');
+                    M.Slider.init(slider, {
+                    	indicators: false,
+                    	interval: 6000,
+                    	height:500,
+                    	duration: 500
+                    });
+
+
+					const parallax = document.querySelectorAll('.parallax');
+                    M.Parallax.init(parallax);
